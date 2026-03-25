7e8db658cbb689ac119e33b1df3a0e738fa151ae
mongdeon <siwho122@naver.com>
2026-03-25 21:21:53 +0900

    fix bot order

--- bot.py
+++ bot.py
@@ -1,11 +1,36 @@
+import discord
+from discord.ext import commands
+import asyncio
+from core.config import TOKEN
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
 @bot.event
 async def on_ready():
-
     try:
         synced = await bot.tree.sync()
         print(f"글로벌 슬래시 동기화 완료: {len(synced)}개")
-
     except Exception as e:
-        print(e)
+        print(f"슬래시 동기화 오류: {e}")
 
     print(f"{bot.user} 온라인")
+
+
+async def main():
+    async with bot:
+        await bot.load_extension("cogs.settings_cog")
+        await bot.load_extension("cogs.recruit_cog")
+        await bot.load_extension("cogs.team_cog")
+        await bot.load_extension("cogs.ranking_cog")
+        await bot.load_extension("cogs.profile_cog")
+        await bot.start(TOKEN)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
